cb0cf82995651bd77ffceb7363a96939a9c4e606
one <one@place.com>
2022-03-27 14:23:56 +0200

    dodani i uredeni login i signup pageovi, integriran firebase.

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,39 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <nav id="nav" class="navbar navbar-expand-lg navbar-light bg-transparent">
+      <a class="navbar-brand" href="#">
+        <img src="@/assets/logo.png" width="100" class="d-inline-block align-top" alt="">
+      </a> 
+        <button class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-targer="#navbarToggler"
+          aria-controls="navbarToggler"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      <div class="collapse navbar-collapse" id="navbarToggler">
+        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+          <li class="nav-item">
+            <router-link class="nav-link" to="/">Home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/login">Login</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/signup">Signup</router-link>
+          </li>
+        </ul>
+      </div>
     </nav>
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -29,4 +54,5 @@ nav {
     }
   }
 }
+
 </style>
